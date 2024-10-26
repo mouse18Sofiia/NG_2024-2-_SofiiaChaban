273e5e60a5102b7f4a446d6aa60bcb37409b97fd
Sofiia Chaban <sonyamouse05@gmail.com>
2024-10-26 21:39:40 +0300

    Corrected the task

--- Lesson_1/Task_3/main.cpp
+++ Lesson_1/Task_3/main.cpp
@@ -4,46 +4,50 @@ using namespace std;
 
 int main()
 {
-    int one , two , act;
+    int one, two, act;
     char symbol;
-    const char* name;
 
-    cout  <<"The first number:" << endl;
+    cout << "The first number:" << endl;
     cin >> one;
-    cout  <<"The second number:" << endl;
+    cout << "The second number:" << endl;
     cin >> two;
-    cout  <<"Please, choose an action:" << endl;
+    cout << "Please, choose an action (+, -, *, /):" << endl;
     cin >> symbol;
 
     switch(symbol)
     {
-        case '+':
-        {
-            act = one+two;
-            name = "Sum";
-            break;
+    case '+':
+    {
+        act = one + two;
+        cout << "Result: " << act << endl;
+        break;
+    }
+    case '-':
+    {
+        act = one - two;
+        cout << "Result: " << act << endl;
+        break;
+    }
+    case '*':
+    {
+        act = one * two;
+        cout << "Result: " << act << endl;
+        break;
+    }
+    case '/':
+    {
+        if (two != 0) {
+            act = one / two;
+            cout << "Result: " << act << endl;
+        } else {
+            cout << "ERROR: Division by zero" << endl;
         }
-        case '-':
-        {
-            act = one-two;
-            name = "Minus";
-            break;
-        }
-        case '*':
-        {
-            act = one*two;
-            name = "Multiply";
-            break;
-        }
-        case '/':
-        {
-            act = one/two;
-            name = "Divide";
-            break;
-        }
-        default: cout << "ERROR" << endl; return -1;
+        break;
+    }
+    default:
+        cout << "ERROR: Invalid operation" << endl;
+        return -1;
     }
 
-    cout << name << " two numbers: " << act << endl;
     return 0;
 }
